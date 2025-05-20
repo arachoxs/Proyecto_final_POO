a8343f6c954ad1f0c962e3c89b4f339dc0c44472
aracho <j.agudelo9@utp.edu.co>
2025-05-20 10:57:07 -0500

    first

---
+++ src/main/java/com/mycompany/robot_modular/Robot_modular.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.robot_modular;
+
+/**
+ *
+ * @author agude
+ */
+public class Robot_modular {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
